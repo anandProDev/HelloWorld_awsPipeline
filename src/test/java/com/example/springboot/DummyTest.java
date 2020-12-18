f2c985d469fad22230a30a139f3a0206d818e621
Anand Elangovan <anand.javapro@gmail.com>
2020-12-18 11:45:46 +0000

    adding build spec yaml file

---
+++ src/test/java/com/example/springboot/DummyTest.java
@@ -0,0 +1,23 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class DummyTest {
+
+	@Test
+	public void getHello() {
+		assertEquals("Hello", "Hello");
+	}
+}
